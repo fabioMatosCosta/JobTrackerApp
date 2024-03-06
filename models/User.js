b8fbcd64fffb063539231a5076a1c4da7d211eed
Fábio Costa <fabiodanielmc@gmail.com>
2024-03-06 22:27:02 +0100

    create user schema

---
+++ models/User.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50,
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(v);
+            },
+            message: (props) => `${props.value} is not a valid password : 
+            - at least 8 characters,
+            - must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number.`,
+        },
+    },
+    picturePath: {
+        type: String,
+        require: true,
+        default: ""
+    },
+    jobPosts: {
+        type: mongoose.Types.ObjectId,
+        ref: "JobPost",
+    },
+    profileLinks:{
+        type: Array,
+        default: []
+    },
+    contacts:{
+        type: mongoose.Types.ObjectId,
+        ref: "Contacts",
+    }
+});
