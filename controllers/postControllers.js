a0af6ef9112cc44bda0cffdd1b6b96edd18da455
Fábio Costa <fabiodanielmc@gmail.com>
2024-03-09 13:22:56 +0100

    first try to create new post function

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -18,11 +18,25 @@ export const getPostList = async (req, res) => {
 /* Create */
 export const createPost =  async (req, res) => {
     try{
+
         const { userId } = req.params;
-        const user = await User.findById(userId);
-
+        const newJobPost = new JobPost({
+            title: req.body.title,
+            type: req.body.type,
+            jobLink: req.body.jobLink,
+            company: req.body.company,
+            companyWebsite: req.body.companyWebsite,
+            userId: user._id,
+            dateToApply: req.body.dateToApply,
+            });
         
+        await newJobPost.save();
 
+        const user = await User.findById(userId);
+        const postIdList = user.jobPosts;
+        const jobPosts = await JobPost.find({ _id: { $in: postIdList } });
+
+        res.status(200).json(jobPosts);
     } catch (err) {
         res.status(409).json({ message: err.message });
     }
